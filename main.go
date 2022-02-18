472346f39f5d04dacdf5d3d3c04e8957f39cbe74
Trygve Ruud <trygvru@stud.ntnu.no>
2022-02-18 15:47:15 +0100

    implemented phoenix

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 )
 
 func main() {
+	phoenix.Init()
 	go phoenix.Phoenix()
 
 	controlunit.Init()
--- phoenix/phoenix.go
+++ phoenix/phoenix.go
@@ -2,32 +2,34 @@ package phoenix
 
 import (
 	"fmt"
-	"os/exec"
 	"os"
+	"os/exec"
 	"time"
 )
 
-func Phoenix() {
+func Init() {
+	fmt.Println("Phoenix is waiting")
+	filename := "phoenix/phoenix.txt"
 	for {
-		filename := "phoenix/phoenix.txt"		
+		time.Sleep(time.Second)
 		file, _ := os.Stat(filename)
 		modifiedtime := file.ModTime()
-		fmt.Println("Last modified time : ", modifiedtime)
+		// fmt.Println("Last modified time : ", modifiedtime)
 
-		data, _ := os.ReadFile(filename)
-		fmt.Println("You recieved: ", string(data))
+		// data, _ := os.ReadFile(filename)
+		// fmt.Println("You recieved: ", string(data))
 
 		if modifiedtime.Add(2 * time.Second).Before(time.Now()) {
 			fmt.Println("Spawning new program ")
 			break
 		}
-		time.Sleep(time.Second)
 	}
-	
-	//
-	cmnd := exec.Command("gnome-terminal", "--", "go","run","./main.go")
-	cmnd.Run()
 
+	cmnd := exec.Command("gnome-terminal", "--", "go", "run", "./main.go")
+	cmnd.Run()
+}
+
+func Phoenix() {
 	for {
 		filename := "phoenix/phoenix.txt"
 		msg := fmt.Sprintln("Writing to file", time.Now())
