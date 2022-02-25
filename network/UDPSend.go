8c339c4ef7a7f006f8f9fbb068be547b3a54cbb1
jogga19 <71340739+jogga19@users.noreply.github.com>
2022-02-25 10:40:05 +0100

    Update UDPSend.go

--- network/UDPSend.go
+++ network/UDPSend.go
@@ -1,16 +1,20 @@
 package network
 
 import (
+	"elevators/filesystem"
+	"encoding/json"
+	"fmt"
 	"net"
+	"time"
 )
 
-const broadcastAddr = "255.255.255.255"
-const port = 20014
+// const broadcastAddr = "255.255.255.255"
+// const port = 20014
 
 func InitUDPSendingSocket(port int, sendAddr string) (net.UDPAddr, *net.UDPConn) {
 	sendaddr := net.UDPAddr{
 		Port: port,
-		IP:   net.ParseIP(broadcastAddr),
+		IP:   net.ParseIP(sendAddr),
 	}
 	wconn, err := net.DialUDP("udp", nil, &sendaddr) // code does not block here
 	if err != nil {
@@ -21,7 +25,7 @@ func InitUDPSendingSocket(port int, sendAddr string) (net.UDPAddr, *net.UDPConn)
 	return sendaddr, wconn
 }
 
-func BroadcastMessage(message string, wconn *net.UDPConn) {
+func BroadcastMessage(message string, wconn *net.UDPConn) {  //Sending string
 	sendMessage := []byte(message)
 	// var buf [1024]byte
 	_, err := wconn.Write(sendMessage)
@@ -29,3 +33,46 @@ func BroadcastMessage(message string, wconn *net.UDPConn) {
 		panic(err)
 	}
 }
+
+// func BroadcastMessage(message json.RawMessage, wconn *net.UDPConn) {  //Sending json.RawMessage
+// 	// sendMessage := []byte(message)
+// 	_, err := wconn.Write(message)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
+
+
+
+
+
+func TestSendAndReceive(){
+	UDPPort := 20014
+
+	var state  filesystem.OrderState
+	state.Dir = "up"
+	state.Floor = 3
+	state.Name = "Elevator"
+
+	jsState, _ := json.Marshal(state)
+	json.Unmarshal(jsState, &state)
+	fmt.Println(string(jsState))
+	// fmt.Println(string(state))
+
+	//Initialize sockets
+	_, wconn := InitUDPSendingSocket(UDPPort, "255.255.255.255")
+	_, conn := InitUDPReceivingSocket(UDPPort)
+
+	//Close sockets when program terminates
+	defer conn.Close()
+	defer wconn.Close()
+
+	//Send and receive message
+	for {
+		// BroadcastMessage(json.RawMessage(`{"precomputed": true}`), wconn)
+		BroadcastMessage(string(jsState), wconn)
+		time.Sleep(time.Millisecond * 2000)
+		ReceiveUDPMessage(conn)
+	}
+}
+
